b8d0e6cdd84533af2c897cb52f0dace4f17db126
Yuri Lopes <devyurilopes@gmail.com>
2023-02-04 12:12:24 +0000

    app todo with pinia vue

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,26 @@
+<script setup>
+import TodoForm from "./TodoForm.vue"
+import TodoList from "./TodoList.vue"
+
+  components: { TodoForm }
+</script>
+
+<template>
+    <div class="todo-app">
+        <h1> To Do List </h1>
+        <todo-form />
+        <todo-list />
+
+    </div>
+</template>
+
+
+<style scoped>
+.todo-app {
+  padding: 5vh;
+  min-height: 100vh;
+  width: 30vw;
+  background-color: #f7f9fc;
+  color: black;
+}
+</style>
